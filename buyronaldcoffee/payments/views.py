e9f4ba007c21ec8d6484c117afb159724b58f497
Reagan Ronald <reaganronashi@gmail.com>
2023-07-11 20:33:41 +0300

    mods

--- buyronaldcoffee/payments/views.py
+++ buyronaldcoffee/payments/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.http import JsonResponse
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'payments/index.html')
+
+def charge(request):
+    amount = 10
+    if request.method == 'POST':
+        print('Data:', request.POST['amount'])
+    
+    return redirect(reverse('success', args=[amount]))
+
+def success_msg(request,args):
+    amount = args
+    return render(request, 'payments/sucess.html', {'amounts':amounts})    
+                  
+
